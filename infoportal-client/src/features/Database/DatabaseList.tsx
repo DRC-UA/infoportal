4b9606c83e26a8709ced7b96a32bebaeba3b34e1
Alexandre Annic <alexandre.annic1@gmail.com>
2024-02-27 12:11:46 +0200

    fix(Database): List filter

--- infoportal-client/src/features/Database/DatabaseList.tsx
+++ infoportal-client/src/features/Database/DatabaseList.tsx
@@ -3,12 +3,12 @@ import {useI18n} from '@/core/i18n'
 import {useNavigate} from 'react-router'
 import {Page, PageTitle} from '@/shared/Page'
 import {Panel} from '@/shared/Panel'
-import {Sheet} from '@/shared/Sheet/Sheet'
 import {KoboFormSdk} from '@/core/sdk/server/kobo/KoboFormSdk'
 import {TableIconBtn} from '@/features/Mpca/MpcaData/TableIcon'
 import React from 'react'
 import {Txt} from 'mui-extension'
 import {databaseIndex} from '@/features/Database/databaseIndex'
+import {Datatable} from '@/shared/Datatable/Datatable'
 
 export const DatabaseList = ({
   forms,
@@ -23,26 +23,57 @@ export const DatabaseList = ({
         <>
           <PageTitle>{m.selectADatabase}</PageTitle>
           <Panel>
-            <Sheet defaultLimit={200} id="kobo-index" onClickRows={_ => navigate(databaseIndex.siteMap.database.absolute(_.serverId, _.id))} data={forms} columns={[
+            <Datatable defaultLimit={200} id="kobo-index" onClickRows={_ => navigate(databaseIndex.siteMap.database.absolute(_.serverId, _.id))} data={forms} columns={[
               {
                 id: 'name',
                 type: 'string',
                 head: m.name,
-                renderValue: _ => KoboFormSdk.parseFormName(_.name)?.name,
-                render: _ => <Txt bold>{KoboFormSdk.parseFormName(_.name)?.name}</Txt>,
+                render: _ => {
+                  return {
+                    label: <Txt bold>{KoboFormSdk.parseFormName(_.name)?.name}</Txt>,
+                    value: KoboFormSdk.parseFormName(_.name)?.name,
+                  }
+                },
               },
-              {id: 'program', type: 'select_one', head: m.program, render: _ => KoboFormSdk.parseFormName(_.name)?.program},
+              {
+                id: 'program',
+                type: 'select_one',
+                head: m.program,
+                renderQuick: _ => KoboFormSdk.parseFormName(_.name)?.program
+              },
               {
                 id: 'donors',
                 head: m.donor,
-                render: _ => KoboFormSdk.parseFormName(_.name)?.donors?.join(',')
+                renderQuick: _ => KoboFormSdk.parseFormName(_.name)?.donors?.join(',')
               },
-              {id: 'createdAt', type: 'date', head: m.createdAt, render: _ => <Txt color="hint">{formatDate(_.createdAt)}</Txt>},
-              {id: 'updatedAt', type: 'date', head: m.updatedAt, render: _ => <Txt color="hint">{formatDate(_.updatedAt)}</Txt>},
               {
-                id: 'actions', width: 0, align: 'right', head: '', render: _ => <>
-                  <TableIconBtn color="primary">chevron_right</TableIconBtn>
-                </>
+                id: 'createdAt',
+                type: 'date',
+                head: m.createdAt,
+                render: _ => {
+                  return {
+                    label: <Txt color="hint">{formatDate(_.createdAt)}</Txt>,
+                    value: _.createdAt,
+                  }
+                }
+              },
+              {
+                id: 'updatedAt',
+                type: 'date',
+                head: m.updatedAt,
+                render: _ => {
+                  return {
+                    label: <Txt color="hint">{formatDate(_.updatedAt)}</Txt>,
+                    value: _.updatedAt,
+                  }
+                }
+              },
+              {
+                id: 'actions',
+                width: 0,
+                align: 'right',
+                head: '',
+                renderQuick: _ => <><TableIconBtn color="primary">chevron_right</TableIconBtn></>
               },
             ]}/>
           </Panel>
