121936d217f7b52fe9274e06242ac7df8da1af0c
Alexandre Annic <alexandre.annic1@gmail.com>
2024-03-04 21:59:10 +0200

    feat(client/AppHeaderFeatures): Handle group

--- infoportal-client/src/shared/Layout/Header/AppHeaderFeatures.tsx
+++ infoportal-client/src/shared/Layout/Header/AppHeaderFeatures.tsx
@@ -1,10 +1,13 @@
 import {IpIconBtn, IpIconBtnProps} from '@/shared/IconBtn'
 import React from 'react'
 import {useSession} from '@/core/Session/SessionContext'
-import {Box, Popover} from '@mui/material'
+import {Box, Grid, Popover, useTheme} from '@mui/material'
 import {useI18n} from '@/core/i18n'
 import {AppFeature, appFeatures} from '@/features/appFeatureId'
 import {FeatureLogo} from '@/features/FeatureLogo'
+import {fnSwitch, Obj, seq} from '@alexandreannic/ts-utils'
+import {styleUtils} from '@/core/theme'
+import {Txt} from 'mui-extension'
 
 const iconSize = 92
 
@@ -12,8 +15,7 @@ export const AppHeaderFeatures = (props: Omit<IpIconBtnProps, 'children'>) => {
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null)
   const {session, accesses} = useSession()
   const open = (!!anchorEl)
-  const {m} = useI18n()
-  const features: AppFeature[] = appFeatures.filter(_ => !_.showIf || _.showIf(session, accesses))
+  const t = useTheme()
   return (
     <>
       <IpIconBtn
@@ -30,19 +32,49 @@ export const AppHeaderFeatures = (props: Omit<IpIconBtnProps, 'children'>) => {
         onClose={() => setAnchorEl(null)}
         open={open}
       >
-        <Box sx={{width: (iconSize + 8) * 3, p: .5}}>
-          {features.map(_ =>
-            <FeatureLogo iconSize={40} key={_.id} feature={_} sx={{
-              display: 'inline-block',
-              height: iconSize,
-              width: iconSize,
-              maxWidth: iconSize,
-              margin: .25,
-              py: 1,
-              px: .5,
-            }}/>
-          )}
+        <Box sx={{width: (iconSize + 8) * 4 + 16, p: .5, pb: 0}}>
+          {new Obj(seq(appFeatures).groupBy(_ => _.category!))
+            .mapValues(features => features.filter(_ => !_.showIf || _.showIf(session, accesses)))
+            .filter((_, features) => features.length > 0)
+            .entries()
+            .map(([category, features]) => (
+              <Box key={category} sx={{p: 1, background: styleUtils(t).color.toolbar, mb: .5, borderRadius: (t.shape.borderRadius - 2) + 'px'}}>
+                <Txt bold uppercase size="small" color="hint" sx={{fontWeight: '600', ml: 1}}>{category}</Txt>
+                <Box>
+                  {features!.map(feature => (
+                    <FeatureLogo
+                      key={feature.id}
+                      feature={feature}
+                      iconSize={40}
+                      sx={{
+                        display: 'inline-block',
+                        height: iconSize,
+                        width: iconSize,
+                        maxWidth: iconSize,
+                        margin: .25,
+                        py: 1,
+                        px: .5,
+                      }}
+                    />
+                  ))}
+                </Box>
+              </Box>
+            ))}
         </Box>
+
+        {/*<Box sx={{width: (iconSize + 8) * 3, p: .5}}>*/}
+        {/*  {features.map(_ =>*/}
+        {/*    <FeatureLogo iconSize={40} key={_.id} feature={_} sx={{*/}
+        {/*      display: 'inline-block',*/}
+        {/*      height: iconSize,*/}
+        {/*      width: iconSize,*/}
+        {/*      maxWidth: iconSize,*/}
+        {/*      margin: .25,*/}
+        {/*      py: 1,*/}
+        {/*      px: .5,*/}
+        {/*    }}/>*/}
+        {/*  )}*/}
+        {/*</Box>*/}
       </Popover>
     </>
   )
