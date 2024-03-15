1f08cb72b98b463b3a6df06c447c1bafb00fa073
Alexandre Annic <alexandre.annic1@gmail.com>
2024-03-15 08:37:02 +0100

    feat(Meta): Restrict some data

--- infoportal-client/src/features/Meta/Table/MetaTable.tsx
+++ infoportal-client/src/features/Meta/Table/MetaTable.tsx
@@ -11,6 +11,7 @@ import {PopoverWrapper} from '@/shared/PopoverWrapper'
 import React, {useMemo} from 'react'
 import {OptionLabelTypeCompact} from '@/shared/customInput/SelectStatus'
 import {useSession} from '@/core/Session/SessionContext'
+import {DatatableColumn} from '@/shared/Datatable/util/datatableType'
 
 type Data = IKoboMeta & {
   duplicatedPhone?: number
@@ -34,6 +35,123 @@ export const MetaTable = () => {
       }
     })
   }, [ctx.data.data])
+
+  const columns = useMemo(() => {
+    const x: DatatableColumn.Props<Data>[] = [
+      {
+        type: 'select_one',
+        id: 'status',
+        head: m.status,
+        width: 0,
+        align: 'center',
+        render: _ => {
+          return {
+            label: <OptionLabelTypeCompact type={koboMetaStatusLabel[_.status!]}/>,
+            value: _.status,
+            option: _.status,
+          }
+        },
+      },
+      {
+        type: 'string',
+        id: 'id',
+        head: m.koboId,
+        renderQuick: _ => _.koboId,
+      },
+      {
+        type: 'date',
+        id: 'date',
+        head: m.date,
+        render: _ => {
+          return {
+            value: _.date,
+            label: formatDate(_.date),
+            tooltip: formatDateTime(_.date),
+          }
+        }
+      },
+      {
+        id: 'formId',
+        type: 'select_one',
+        head: m.koboForm,
+        renderQuick: _ => KoboIndex.searchById(_.formId)?.translation ?? _.formId,
+      },
+      {
+        id: 'oblast',
+        type: 'select_one',
+        head: m.oblast,
+        renderQuick: _ => _.oblast,
+      },
+      {
+        id: 'sector',
+        type: 'select_one',
+        head: m.sector,
+        renderQuick: _ => _.sector,
+      },
+      {
+        id: 'activity',
+        type: 'select_one',
+        head: m.activity,
+        renderQuick: _ => _.activity,
+      },
+      {
+        id: 'project',
+        type: 'select_multiple',
+        head: m.project,
+        options: () => DatatableUtils.buildOptionByEnum(DrcProject),
+        renderQuick: _ => _.project ?? [],
+      },
+      {
+        id: 'raion',
+        type: 'select_one',
+        head: m.raion,
+        renderQuick: _ => _.raion,
+      },
+      {
+        id: 'hromada',
+        type: 'select_one',
+        head: m.hromada,
+        renderQuick: _ => _.hromada,
+      },
+      {
+        type: 'string',
+        id: 'taxId',
+        head: m.taxID,
+        renderQuick: _ => _.taxId,
+      },
+      {
+        type: 'number',
+        id: 'duplicatedTax',
+        head: m.taxIdOccurrences,
+        renderQuick: _ => _.duplicatedTax,
+      },
+      {
+        type: 'string',
+        id: 'phone',
+        head: m.phone,
+        renderQuick: _ => _.phone ? session.admin ? _.phone : '*******' + _.phone.substring(_.phone.length - 3) : undefined,
+      },
+      {
+        type: 'number',
+        id: 'duplicatedPhone',
+        head: m.phoneOccurrences,
+        renderQuick: _ => _.duplicatedPhone,
+      },
+      ...session.admin ? [{
+        type: 'select_one',
+        id: 'enumerator',
+        head: m.enumerator,
+        renderQuick: _ => _.enumerator,
+      } as DatatableColumn.Props<Data>] : [],
+      {
+        type: 'number',
+        id: 'individuals',
+        head: m.individuals,
+        renderQuick: _ => _.personsCount,
+      },
+    ]
+    return x
+  }, [session, mappedData])
 
   return (
     <Page width="full">
@@ -62,113 +180,7 @@ export const MetaTable = () => {
           )}
           id="meta"
           data={mappedData}
-          columns={[
-            {
-              type: 'select_one',
-              id: 'status',
-              head: m.status,
-              width: 0,
-              align: 'center',
-              render: _ => {
-                return {
-                  label: <OptionLabelTypeCompact type={koboMetaStatusLabel[_.status!]}/>,
-                  value: _.status,
-                  option: _.status,
-                }
-              },
-            },
-            {
-              type: 'string',
-              id: 'id',
-              head: m.koboId,
-              renderQuick: _ => _.koboId,
-            },
-            {
-              type: 'date',
-              id: 'date',
-              head: m.date,
-              render: _ => {
-                return {
-                  value: _.date,
-                  label: formatDate(_.date),
-                  tooltip: formatDateTime(_.date),
-                }
-              }
-            },
-            {
-              id: 'formId',
-              type: 'select_one',
-              head: m.koboForm,
-              renderQuick: _ => KoboIndex.searchById(_.formId)?.translation ?? _.formId,
-            },
-            {
-              id: 'oblast',
-              type: 'select_one',
-              head: m.oblast,
-              renderQuick: _ => _.oblast,
-            },
-            {
-              id: 'sector',
-              type: 'select_one',
-              head: m.sector,
-              renderQuick: _ => _.sector,
-            },
-            {
-              id: 'activity',
-              type: 'select_one',
-              head: m.activity,
-              renderQuick: _ => _.activity,
-            },
-            {
-              id: 'project',
-              type: 'select_multiple',
-              head: m.project,
-              options: () => DatatableUtils.buildOptionByEnum(DrcProject),
-              renderQuick: _ => _.project ?? [],
-            },
-            {
-              id: 'raion',
-              type: 'select_one',
-              head: m.raion,
-              renderQuick: _ => _.raion,
-            },
-            {
-              id: 'hromada',
-              type: 'select_one',
-              head: m.hromada,
-              renderQuick: _ => _.hromada,
-            },
-            {
-              type: 'string',
-              id: 'taxId',
-              head: m.taxID,
-              renderQuick: _ => _.taxId,
-            },
-            {
-              type: 'number',
-              id: 'duplicatedTax',
-              head: m.taxIdOccurrences,
-              renderQuick: _ => _.duplicatedTax,
-            },
-            {
-              type: 'string',
-              id: 'phone',
-              head: m.phone,
-              renderQuick: _ => _.phone,
-            },
-            {
-              type: 'number',
-              id: 'duplicatedPhone',
-              head: m.phoneOccurrences,
-              renderQuick: _ => _.duplicatedPhone,
-            },
-            {
-              type: 'number',
-              id: 'individuals',
-              head: m.individuals,
-              renderQuick: _ => _.personsCount,
-            },
-          ]}
+          columns={columns}
         />
       </Panel>
     </Page>
