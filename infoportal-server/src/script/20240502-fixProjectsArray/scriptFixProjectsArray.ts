25777dfd4901ad5e3513b09a2100b58a41d5c1d5
Alexandre Annic <alexandre.annic1@gmail.com>
2024-05-02 12:08:52 +0300

    feat: scripts projects

--- infoportal-server/src/script/20240502-fixProjectsArray/scriptFixProjectsArray.ts
+++ infoportal-server/src/script/20240502-fixProjectsArray/scriptFixProjectsArray.ts
@@ -3,22 +3,21 @@ import {PromisePool} from '@supercharge/promise-pool'
 
 export const runFixProjectsArray = async (prisma: PrismaClient) => {
   const answers = await prisma.koboAnswers.findMany({where: {formId: 'aTP5nwZjpyR7oy7bdMZktC'}})
-  // console.log(answers.filter(_ => {
-  //   if (!_.tags || !Array.isArray((_.tags as any).project)) return false
-  //   let projects: string[] = (_.tags as any).project as any
-  //   return projects.find((p: string) => p.includes('["'))
-  // }).map(_ => _.tags))
-  // console.log(answers.filter((_: any) => _.tags && !Array.isArray(_.tags.project)).map(_ => _.tags))
+  console.log(answers.filter(_ => {
+    if (!_.tags || !Array.isArray((_.tags as any).project)) return false
+    let projects: string[] = (_.tags as any).project as any
+    return projects.find((p: string) => p.includes('["'))
+  }).map(_ => _.tags))
 
-  const fixed = answers.filter((_: any) => {
-    if (!_.tags) return
-    if (!_.tags.project) return
-    // if (typeof _.tags.project === 'string') return true
-    return !Array.isArray(_.tags.project)
-  }).map(_ => {
-    // (_ as any).tags.project = ['UKR-000322 ECHO2']
-    return _
-  })
+  // const fixed = answers.filter((_: any) => {
+  //   if (!_.tags) return
+  //   if (!_.tags.project) return
+  //   if (!Array.isArray(_.tags.project)) return
+  //   return _.tags.project.length === 1 && _.tags.project[0] === '["UKR-000298 Novo-Nordisk", "UKR-000314 UHF4"]'
+  // }).map(_ => {
+  //   (_ as any).tags.project = ["UKR-000298 Novo-Nordisk", "UKR-000314 UHF4"]
+  //   return _
+  // })
   // await PromisePool.withConcurrency(20).for(fixed).process(f => {
   //   console.log('updating... ' + f.id)
   //   return prisma.koboAnswers.update({
@@ -26,6 +25,6 @@ export const runFixProjectsArray = async (prisma: PrismaClient) => {
   //     data: {tags: f.tags as any},
   //   })
   // })
-  console.log(fixed.map(_ => _.tags))
-  console.log(fixed.length)
+  // console.log(fixed.map(_ => _.tags))
+  // console.log(fixed.length)
 }
