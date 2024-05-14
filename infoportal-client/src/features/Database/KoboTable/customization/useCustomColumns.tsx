ea05289d1c2dbfad8845c39282080439db4cf8a1
marsi <mashagal14@gmail.com>
2024-05-14 13:10:38 +0300

    new beneficiaries in custom column for gbv

--- infoportal-client/src/features/Database/KoboTable/customization/useCustomColumns.tsx
+++ infoportal-client/src/features/Database/KoboTable/customization/useCustomColumns.tsx
@@ -186,23 +186,6 @@ export const useCustomColumns = (): DatatableColumn.Props<KoboMappedAnswer>[] =>
         lastStatusUpdate,
       ]
     }
-    const beneficiaries:DatatableColumn.Props<any>[] = [
-      {
-        id: 'beneficiaries',
-        head: m.beneficiaries,
-        type: 'number',
-        renderQuick: (row: KoboAnswerFlat<Protection_gbv.T, any>) => {
-          if (row.new_ben === 'yes') {
-            return row.numb_part || 0;
-          } else if (row.new_ben === 'bno' && row.hh_char_hh_det) {
-            return row.hh_char_hh_det.reduce((count, participant) => {
-              return count + (participant.hh_char_hh_new_ben === 'yes' ? 1 : 0);
-            }, 0);
-          }
-          return 0;
-        }
-      }
-]
     const ecrecScore: DatatableColumn.Props<any>[] = [
       {
         id: 'vulnerability_sore',
@@ -265,7 +248,21 @@ export const useCustomColumns = (): DatatableColumn.Props<KoboMappedAnswer>[] =>
         }
       ],
       [KoboIndex.byName('protection_gbv').id]: [
-        ...beneficiaries
+        {
+          id: 'beneficiaries',
+          head: m.beneficiaries,
+          type: 'number',
+          renderQuick: (row: KoboAnswerFlat<Protection_gbv.T, any>) => {
+            if (row.new_ben === 'yes') {
+              return row.numb_part || 0
+            } else if (row.new_ben === 'bno' && row.hh_char_hh_det) {
+              return row.hh_char_hh_det.reduce((count, participant) => {
+                return count + (participant.hh_char_hh_new_ben === 'yes' ? 1 : 0)
+              }, 0)
+            }
+            return 0
+          }
+        }
       ],
       [KoboIndex.byName('shelter_cashForRepair').id]: [
         ...paymentStatusShelter(),
