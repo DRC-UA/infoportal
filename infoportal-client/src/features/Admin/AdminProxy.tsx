b006d88a3446943d0927f0078cb3b7af504d0f31
marsi <mashagal14@gmail.com>
2024-05-15 12:38:16 +0300

    delete <sheet/>

--- infoportal-client/src/features/Admin/AdminProxy.tsx
+++ infoportal-client/src/features/Admin/AdminProxy.tsx
@@ -15,6 +15,7 @@ import {TableIconBtn} from '@/features/Mpca/MpcaData/TableIcon'
 import {Proxy} from '@/core/sdk/server/proxy/Proxy'
 import {endOfDay} from 'date-fns'
 import {Modal} from 'mui-extension/lib/Modal'
+import {Datatable} from '@/shared/Datatable/Datatable'
 
 interface CreateForm {
   name: string
@@ -46,7 +47,7 @@ export const AdminProxy = () => {
   return (
     <Page width="lg">
       <Panel>
-        <Sheet
+        <Datatable
           id="proxy"
           header={
             <>
@@ -104,54 +105,71 @@ export const AdminProxy = () => {
               type: 'string',
               id: 'name',
               head: m.name,
-              renderValue: _ => _.name,
-              render: _ => _.name,
+              renderQuick: _ => _.name,
             },
             {
               type: 'string',
               id: 'origin',
               head: m.origin,
-              renderValue: _ => _.slug,
-              render: _ => <Txt link><a target="_blank" href={Proxy.makeUrl(_)}>{Proxy.makeUrl(_)}</a></Txt>,
+              render: _ => {
+                return {
+                  value: m.origin,
+                  label: (
+                    <Txt link><a target="_blank" href={Proxy.makeUrl(_)}>{Proxy.makeUrl(_)}</a></Txt>
+                  )
+                }
+              }
             },
             {
               type: 'string',
               id: 'destination',
               head: m.destination,
-              renderValue: _ => _.url,
-              render: _ => <Txt link><a target="_blank" href={_.url}>{_.url}</a></Txt>,
+              render: _ => {
+                return {
+                  value: m.destination,
+                  label: (
+                    <Txt link><a target="_blank" href={_.url}>{_.url}</a></Txt>
+                  )
+                }
+              }
             },
             {
               type: 'date',
               id: 'createdAt',
               head: m.createdAt,
-              renderValue: _ => _.createdAt,
-              render: _ => formatDate(_.createdAt),
+              renderQuick: _ => formatDate(_.createdAt),
             },
             {
               type: 'date',
               id: 'expireAt',
               width: 0,
               head: m.expireAt,
-              renderValue: _ => _.expireAt,
-              render: _ => _.expireAt && formatDate(_.expireAt),
+              renderQuick: _ => _.expireAt && formatDate(_.expireAt),
             },
             {
               type: 'string',
               id: 'enabled',
               align: 'center',
               head: m.enabled,
-              renderValue: _ => _.disabled ? 'false' : 'true',
-              render: _ => (
-                <Switch checked={!_.disabled} onChange={e => _search.update(_.id, {disabled: !e.currentTarget.checked})}/>
-              ),
+              render: _ => {
+                return {
+                  label: (
+                    <Switch
+                      checked={!_.disabled}
+                      onChange={e => _search.update(_.id, {disabled: !e.currentTarget.checked})}
+                    />
+                  ),
+                  value: !_.disabled ? 'Enabled' : 'Disabled',
+                  option: !_.disabled,
+                }
+              }
             },
             {
               id: 'actions',
               head: '',
               width: 0,
               align: 'right',
-              render: _ => (
+              renderQuick: _ => (
                 <TableIconBtn onClick={() => _search.remove(_.id)} loading={_search.removing(_.id)}>delete</TableIconBtn>
               ),
             },
