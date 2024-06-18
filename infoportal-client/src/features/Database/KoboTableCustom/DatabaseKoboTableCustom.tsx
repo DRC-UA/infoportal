396906944c505f67a8b6a84cb20a8fca437ae224
Alexandre Annic <alexandre.annic1@gmail.com>
2024-06-18 16:35:02 +0300

    ok

--- infoportal-client/src/features/Database/KoboTableCustom/DatabaseKoboTableCustom.tsx
+++ infoportal-client/src/features/Database/KoboTableCustom/DatabaseKoboTableCustom.tsx
@@ -5,6 +5,9 @@ import {useKoboSchemaContext} from '@/features/KoboSchema/KoboSchemaContext'
 import {useKoboAnswersContext} from '@/core/context/KoboAnswers'
 import {useParams} from 'react-router'
 import * as yup from 'yup'
+import {useMemoFn} from '@alexandreannic/react-hooks-lib'
+import {seq} from '@alexandreannic/ts-utils'
+import {KoboAnswerId, KoboId} from '@infoportal-common'
 
 export const customForms = [{
   id: '1',
@@ -21,10 +24,8 @@ const urlValidation = yup.object({
 
 export const DatabaseTableCustomRoute = () => {
   const {id} = urlValidation.validateSync(useParams())
-  const formIds = useMemo(
-    () => customForms.find(_ => _.id === id)!.horizontalJoin.map(_ => _.id),
-    [id]
-  )
+  const customForm = useMemo(() => customForms.find(_ => _.id === id), [id])
+  const formIds = useMemo(() => customForm!.horizontalJoin.map(_ => _.id), [id])
   const ctxSchema = useKoboSchemaContext()
   const ctxAnswers = useKoboAnswersContext()
   useEffect(() => {
@@ -33,6 +34,28 @@ export const DatabaseTableCustomRoute = () => {
       ctxSchema.fetchById(_)
     })
   }, [formIds])
+
+  const data = useMemoFn(formIds.map(_ => ctxAnswers.byId.get(_)), dataSets => {
+    if (dataSets.find(_ => !_)) return
+    const indexes: Record<KoboId, Record<KoboAnswerId, any[]>> = {} as any
+    seq(dataSets).compact().forEach((_, i) => {
+      if (i === 0) return
+      const join = customForm!.horizontalJoin?.[i]
+      if (!join) return
+      indexes[formIds[i]] = seq(_.data).groupBy(_ => (_ as any)[join.colName])
+    })
+    return dataSets[0]!.data.map(_ => {
+      return {
+        [formIds[0]]: _,
+        ...seq(formIds).reduceObject(_ => [_, indexes[_]])
+
+      }
+    })
+  })
+
+  // const data = useMemo(() => {
+  //
+  // }, formIds.map(_ => ctxAnswers.byId.get(_)))
   const loading = ctxSchema.anyLoading || !!formIds.find(_ => ctxAnswers.byId.loading(_))
   return (
     <>
