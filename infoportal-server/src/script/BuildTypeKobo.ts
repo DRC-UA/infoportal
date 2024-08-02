a478054d01c260e0c5616945c23abe54bf90dba9
marsi <mashagal14@gmail.com>
2024-08-02 10:26:03 +0300

    feat(BN) Age Group custom header for RRM V2

--- infoportal-server/src/script/BuildTypeKobo.ts
+++ infoportal-server/src/script/BuildTypeKobo.ts
@@ -109,7 +109,10 @@ export class BuildKoboType {
       formId: KoboIndex.byName('bn_rapidResponse').id
     },
     bn_rapidResponse2: {
-      formId: KoboIndex.byName('bn_rapidResponse2').id
+      formId: KoboIndex.byName('bn_rapidResponse2').id, skipQuestionTyping: [
+        'ben_det_hromada',
+        'ben_det_raion',
+      ]
     },
     shelter_cashForRepair: {
       formId: KoboIndex.byName('shelter_cashForRepair').id
