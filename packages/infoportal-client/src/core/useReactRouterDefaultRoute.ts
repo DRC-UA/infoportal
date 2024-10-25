296797d3a436b5fc1d5119e4d605d0a01410717f
Alexandre Annic <alexandre.annic1@gmail.com>
2024-10-25 11:35:16 +0300

    [fix][client] Default react-router override

--- packages/infoportal-client/src/core/useReactRouterDefaultRoute.ts
+++ packages/infoportal-client/src/core/useReactRouterDefaultRoute.ts
@@ -1,13 +1,14 @@
 import {useEffect} from 'react'
-import {useNavigate} from 'react-router-dom'
+import {useLocation, useNavigate} from 'react-router-dom'
 
 /**
  * Workaround since we cannot use <Route index ...> since we put <HashRouter> in _app.tsx.
  * @param route
  */
 export const useReactRouterDefaultRoute = (route: string) => {
+  const loc = useLocation()
   const navigate = useNavigate()
   useEffect(() => {
-    navigate(route)
+    if (loc.pathname === '/') navigate(route)
   }, [])
 }
