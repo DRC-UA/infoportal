1b34cc2bd133d66de469e43d5a894ff03a29f35e
Alexandre Annic <alexandre.annic1@gmail.com>
2025-01-24 07:52:02 -0500

    [feat][scripts] Scripts fixing Kobo server migration (#146)

--- packages/infoportal-scripts/src/appConf.ts
+++ packages/infoportal-scripts/src/appConf.ts
@@ -10,7 +10,7 @@ export const appConf = {
   db: {
     maxConcurrency: e(int, defaultValue(50))('DATABASE_MAX_CONCURRENCY'),
     url: e(required)('DATABASE_URL'),
-    backgrupUrl: e()('DATABASE_URL_BACKUP')
+    backgrupUrl: e()('DATABASE_URL_BACKUP'),
   },
   activityInfo: {
     apiToken: e(required)('ACTIVITY_INFO_API_TOKEN'),
@@ -22,9 +22,16 @@ export const appConf = {
   //   port: e(int, defaultValue(5432))('DB_PORT')
   // },
   kobo: {
-    url: e(defaultValue('https://kobo.humanitarianresponse.info'))('KOBO_URL'),
-    urlV1: e(defaultValue('https://kc-eu.kobotoolbox.org'))('KOBO_URLV1'),
-    token: e(required)('KOBO_TOKEN'),
+    humanitarian: {
+      url: e(defaultValue('https://kobo.humanitarianresponse.info'))('KOBO_HUMANITARIAN_URL'),
+      urlV1: e(defaultValue('https://kc-eu.kobotoolbox.org'))('KOBO_HUMANITARIAN_URLV1'),
+      token: e(required)('KOBO_HUMANITARIAN_TOKEN'),
+    },
+    drc: {
+      url: e(defaultValue('https://kobo.humanitarianresponse.info'))('KOBO_DRC_URL'),
+      urlV1: e(defaultValue('https://kc-eu.kobotoolbox.org'))('KOBO_DRC_URLV1'),
+      token: e(required)('KOBO_DRC_TOKEN'),
+    },
   },
 }
 
--- packages/infoportal-scripts/src/index.ts
+++ packages/infoportal-scripts/src/index.ts
@@ -3,14 +3,20 @@ import {appConf} from './appConf'
 import winston from 'winston'
 import {FixKoboMigration} from './kobo-migration/20250113-fixKoboMigration'
 
-export const koboSdk = new KoboClient({
-  urlv1: appConf.kobo.urlV1 + '/api/v1',
-  urlv2: appConf.kobo.url + '/api',
-  token: appConf.kobo.token,
+export const koboSdkHumanitarian = new KoboClient({
+  urlv1: appConf.kobo.humanitarian.urlV1 + '/api/v1',
+  urlv2: appConf.kobo.humanitarian.url + '/api',
+  token: appConf.kobo.humanitarian.token,
+  log: winston.createLogger(),
+})
+
+export const koboSdkDrc = new KoboClient({
+  urlv1: appConf.kobo.drc.urlV1 + '/api/v1',
+  urlv2: appConf.kobo.drc.url + '/api',
+  token: appConf.kobo.drc.token,
   log: winston.createLogger(),
 })
 ;(async () => {
-  await FixKoboMigration.MissingSubmissions.run()
   // await new BuildKoboType().build('partner_misto_syly')
   // await ActivityInfoBuildType.fslc()
   // await new BuildKoboType().build('ecrec_msme_bha388')
