cf7dd957f9e93cad8c408dbfb6473a70fb3e7be0
Alexandre Annic <alexandre.annic1@gmail.com>
2025-03-05 14:36:10 -0500

    Fix GBV

--- packages/infoportal-client/src/features/ActivityInfo/Gbv/AiGbvMapper.ts
+++ packages/infoportal-client/src/features/ActivityInfo/Gbv/AiGbvMapper.ts
@@ -34,87 +34,94 @@ export namespace AiGbvMapper2 {
         .then((_) => mapActivity(_, periodStr))
     }
 
-  const mapActivity = (data: IKoboMeta[], periodStr: string): Bundle[] => {
+  const mapActivity = async (data: IKoboMeta[], periodStr: string): Promise<Bundle[]> => {
     const res: Bundle[] = []
     let i = 0
-    groupBy({
-      data,
-      groups: [
-        {by: (_) => _.oblast!},
-        {by: (_) => _.raion!},
-        {by: (_) => _.hromada!},
-        {by: (_) => _.settlement!},
-        {by: (_) => _.project?.[0]!},
-      ],
-      finalTransform: async (grouped, [oblast, raion, hromada, settlement, project]) => {
-        const activity: AiGbvType = {
-          'Plan/Project code': planCode[project],
-          'Reporting Organization': 'Danish Refugee Council (DRC)',
-          'Response Theme': 'No specific theme',
-          Oblast: oblast,
-          Raion: raion,
-          Hromada: hromada,
-          Settlement: settlement,
-          // 'Activities and People': mapSubActivity(grouped, periodStr),
-        }
-        const subActivities = mapSubActivity(grouped, periodStr)
-        const activityPrebuilt = {
-          ...activity,
-          ...AiMapper.getLocationRecordIdByMeta({oblast, raion, hromada, settlement}),
-          'Activities and People': subActivities,
-        }
-        const recordId = ActivityInfoSdk.makeRecordId({
-          prefix: 'drcgbv',
-          periodStr,
-          index: i++,
-        })
-        return mapSubActivity(grouped, periodStr).map((subActivity) => {
-          res.push({
-            activity,
-            data: grouped,
-            requestBody: ActivityInfoSdk.wrapRequest(AiGbvType.buildRequest(activityPrebuilt, recordId)),
-            subActivity,
-            recordId,
-            submit: checkAiValid(
-              activity.Oblast,
-              activity.Raion,
-              activity.Hromada,
-              activity.Settlement,
-              activity['Plan/Project code'],
-              ...(activity['Activities and People']?.map((_: AiGbvType.AiTypeActivitiesAndPeople) => _.Indicators) ??
-                []),
-            ),
+    await Promise.all(
+      groupBy({
+        data,
+        groups: [
+          {by: (_) => _.oblast!},
+          {by: (_) => _.raion!},
+          {by: (_) => _.hromada!},
+          {by: (_) => _.settlement!},
+          {by: (_) => _.project?.[0]!},
+        ],
+        finalTransform: async (grouped, [oblast, raion, hromada, settlement, project]) => {
+          const activity: AiGbvType = {
+            'Plan/Project code': planCode[project],
+            'Reporting Organization': 'Danish Refugee Council (DRC)',
+            'Response Theme': 'No specific theme',
+            Oblast: oblast,
+            Raion: raion,
+            Hromada: hromada,
+            Settlement: settlement,
+          }
+          const subActivities = mapSubActivity(grouped, periodStr)
+          const activityPrebuilt = {
+            ...activity,
+            ...AiMapper.getLocationRecordIdByMeta({oblast, raion, hromada, settlement}),
+            'Activities and People': subActivities.map((_) => _.activity),
+          }
+          const recordId = ActivityInfoSdk.makeRecordId({
+            prefix: 'drcgbv',
+            periodStr,
+            index: i++,
           })
-        })
-      },
-    })
+          return subActivities.map((subActivity) => {
+            res.push({
+              activity,
+              data: subActivity.data,
+              requestBody: ActivityInfoSdk.wrapRequest(AiGbvType.buildRequest(activityPrebuilt, recordId)),
+              subActivity: subActivity.activity,
+              recordId,
+              submit: checkAiValid(
+                activity.Oblast,
+                activity.Raion,
+                activity.Hromada,
+                activity.Settlement,
+                activity['Plan/Project code'],
+                ...(activity['Activities and People']?.map((_: AiGbvType.AiTypeActivitiesAndPeople) => _.Indicators) ??
+                  []),
+              ),
+            })
+          })
+        },
+      }).transforms,
+    )
     return res
   }
 
-  const mapSubActivity = (data: IKoboMeta[], periodStr: string): AiGbvType.AiTypeActivitiesAndPeople[] => {
-    const res: AiGbvType.AiTypeActivitiesAndPeople[] = []
+  const mapSubActivity = (
+    data: IKoboMeta[],
+    periodStr: string,
+  ): {activity: AiGbvType.AiTypeActivitiesAndPeople; data: IKoboMeta[]}[] => {
+    const res: {activity: AiGbvType.AiTypeActivitiesAndPeople; data: IKoboMeta[]}[] = []
     groupBy({
       data,
       groups: [{by: (_) => _.activity!}, {by: (_) => _.displacement!}],
       finalTransform: (grouped, [activity, displacement]) => {
         const disaggregation = AiMapper.disaggregatePersons(grouped.flatMap((_) => _.persons).compact())
         res.push({
-          'Non-individuals Reached/Quantity': grouped.length,
-          'Population Group': AiMapper.mapPopulationGroup(displacement),
-          'Reporting Month': periodStr === '2025-01' ? '2025-02' : periodStr,
-          Indicators: match(activity)
-            .cases({
-              WGSS: 'Support through Women and Girls Safe Spaces (WGSS) > # of women and girls who participated in skill-building, recreational, or livelihood (including vocational education) activities in women and girls safe spaces',
-              DignityKits:
-                'Dignity kits to GBV survivors and those at-risk > # of women and girls at risk who received dignity kits',
-              AwarenessRaisingSession:
-                'Conduct awareness raising campaigns on GBV > # of individuals reached with awareness-raising activities and GBV-lifesaving information',
-              PSS: 'GBV hotline > # of individuals who received services through hotlines (excluding PSS and legal aid)',
-              CapacityBuilding:
-                'Capacity Building of GBV service providers to deliver in accordance with the GBViE minimum standards > # of GBV service providers trained to deliver services in accordance with the GBViE minimum standards',
-            })
-            .default(() => aiInvalidValueFlag as any),
-          ...disaggregation,
+          data: grouped,
+          activity: {
+            'Non-individuals Reached/Quantity': grouped.length,
+            'Population Group': AiMapper.mapPopulationGroup(displacement),
+            'Reporting Month': periodStr === '2025-01' ? '2025-02' : periodStr,
+            Indicators: match(activity)
+              .cases({
+                WGSS: 'Support through Women and Girls Safe Spaces (WGSS) > # of women and girls who participated in skill-building, recreational, or livelihood (including vocational education) activities in women and girls safe spaces',
+                DignityKits:
+                  'Dignity kits to GBV survivors and those at-risk > # of women and girls at risk who received dignity kits',
+                AwarenessRaisingSession:
+                  'Conduct awareness raising campaigns on GBV > # of individuals reached with awareness-raising activities and GBV-lifesaving information',
+                PSS: 'GBV hotline > # of individuals who received services through hotlines (excluding PSS and legal aid)',
+                CapacityBuilding:
+                  'Capacity Building of GBV service providers to deliver in accordance with the GBViE minimum standards > # of GBV service providers trained to deliver services in accordance with the GBViE minimum standards',
+              })
+              .default(() => aiInvalidValueFlag as any),
+            ...disaggregation,
+          },
         })
       },
     })
