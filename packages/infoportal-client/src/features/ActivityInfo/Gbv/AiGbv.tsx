2bf2924340a645cbcad00e77bab78c0db8c2d3b9
Pavlo Boiko <pavlo@boiko.work>
2025-03-19 16:57:27 +0200

    [fix][feat][activity info] TEMP
    code

--- packages/infoportal-client/src/features/ActivityInfo/Gbv/AiGbv.tsx
+++ packages/infoportal-client/src/features/ActivityInfo/Gbv/AiGbv.tsx
@@ -1,11 +1,14 @@
-import {useAppSettings} from '@/core/context/ConfigContext'
 import React from 'react'
-import {AiBundleTable} from '@/features/ActivityInfo/shared/AiTable'
+
 import {Period} from 'infoportal-common'
+
+import {useAppSettings} from '@/core/context/ConfigContext'
+import {AiBundleTable} from '@/features/ActivityInfo/shared/AiTable'
 import {Panel} from '@/shared/Panel'
 import {Page} from '@/shared/Page'
 import {useFetcher} from '@/shared/hook/useFetcher'
-import {AiGbvMapper2} from '@/features/ActivityInfo/Gbv/AiGbvMapper'
+
+import {AiGbvMapper2} from './AiGbvMapper'
 
 export const AiGbv = () => {
   const {api} = useAppSettings()
