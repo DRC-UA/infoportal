bfb90f2d9bf1b4fe83eddd020680a49585c6c0ec
mashg <mashagal14@gmail.com>
2025-03-27 12:34:40 +0200

    [pdm][meal] changes to pdm (#181)

--- packages/infoportal-client/src/features/Meal/Winter/MealWinterizationDashboard.tsx
+++ packages/infoportal-client/src/features/Meal/Winter/MealWinterizationDashboard.tsx
@@ -4,7 +4,7 @@ import {useI18n} from '@/core/i18n'
 import {PeriodPicker} from '@/shared/PeriodPicker/PeriodPicker'
 import {ChartBarMultipleBy} from '@/shared/charts/ChartBarMultipleBy'
 import {DebouncedInput} from '@/shared/DebouncedInput'
-import {Div, SlidePanel} from '@/shared/PdfLayout/PdfSlide'
+import {Div, SlidePanel, SlideWidget} from '@/shared/PdfLayout/PdfSlide'
 import {ChartPieWidgetBy} from '@/shared/charts/ChartPieWidgetBy'
 import {DataFilter} from '@/shared/DataFilter/DataFilter'
 import {MapSvgByOblast} from '@/shared/maps/MapSvgByOblast'
@@ -16,7 +16,10 @@ import {ChartBarSingleBy} from '@/shared/charts/ChartBarSingleBy'
 import {useMealWinterizationContext} from '@/features/Meal/Winter/MealWinterizationContext'
 import {appConfig} from '@/conf/AppConfig'
 import {Panel, PanelBody} from '@/shared/Panel'
-import {AgeGroupTable} from '@/shared'
+import {AgeGroupTable, Lazy} from '@/shared'
+import {Box} from '@mui/material'
+import {ChartPieWidget} from '@/shared/charts/ChartPieWidget'
+import {formatLargeNumber} from '@/core/i18n/localization/en'
 
 export interface DashboardPageProps {
   filters: Record<string, string[]>
@@ -98,7 +101,169 @@ export const MealWinterizationDashboard = () => {
       {data && (
         <>
           <Div responsive>
-            <Div column sx={{maxHeight: '50%'}}>
+            <Div column>
+              <SlidePanel>
+                <Box display="flex" justifyContent="space-between" alignItems="center" gap={4}>
+                  {[
+                    {
+                      icon: 'location_on',
+                      label: m.coveredOblasts,
+                      valueFn: () => {
+                        const values = seq(data).map(_ => _.ben_det_oblast).compact()
+                        return values.distinct(_ => _).length
+                      },
+                    },
+                    {
+                      icon: 'location_on',
+                      label: m.coveredRaions,
+                      valueFn: () => {
+                        const values = seq(data).map(_ => _.ben_det_raion).compact()
+                        return values.distinct(_ => _).length
+                      },
+                    },
+                    {
+                      icon: 'fingerprint',
+                      label: m.uniqIndividuals,
+                      valueFn: () => {
+                        const values = seq(data).map(_ => _.unique_number).compact()
+                        return values.distinct(_ => _).length
+                      },
+                    },
+                  ].map(({icon, label, valueFn}) => (
+                    <Box key={label} display="flex" flexDirection="column" alignItems="center" flex={1}>
+                      <Lazy deps={[data]} fn={valueFn}>
+                        {(value) => (
+                          <Box display="flex" alignItems="center" gap={1}>
+                            <span className="material-icons" style={{fontSize: 20, color: '#555'}}>{icon}</span>
+                            <strong style={{fontSize: 18}}>{formatLargeNumber(value)}</strong>
+                          </Box>
+                        )}
+                      </Lazy>
+                      <Box fontSize={12} mt={0.5} textAlign="center">
+                        {label.toUpperCase()}
+                      </Box>
+                    </Box>
+                  ))}
+                </Box>
+              </SlidePanel>
+              <Panel savableAsImg expendable title={m.location}>
+                <PanelBody>
+                  <MapSvgByOblast
+                    sx={{maxWidth: 480, margin: 'auto'}}
+                    fillBaseOn="value"
+                    data={data}
+                    getOblast={(_) => mapOblast[_.ben_det_oblast!]}
+                    value={(_) => true}
+                    base={(_) => _.ben_det_oblast !== undefined}
+                  />
+                </PanelBody>
+              </Panel>
+              <Panel title={m.ageGroup}>
+                <PanelBody>
+                  <AgeGroupTable
+                    tableId="pdm-dashboard"
+                    persons={persons}
+                    enableDisplacementStatusFilter
+                    enablePwdFilter
+                  />
+                </PanelBody>
+              </Panel>
+              <SlidePanel>
+                <SlidePanel title={m.project}>
+                  <ChartBarSingleBy data={data} by={(_) => _.donor} label={Meal_winterizationPdm.options.donor} />
+                </SlidePanel>
+                <SlidePanel title={m.mealMonitoringPdm.pdmType}>
+                  <ChartBarSingleBy data={data} by={(_) => _.pdmtype} label={Meal_winterizationPdm.options.pdmtype} />
+                </SlidePanel>
+              </SlidePanel>
+              <SlidePanel>
+              <SlidePanel title={m.mealMonitoringPdm.priorityNeeds}>
+                <ChartBarMultipleBy
+                  data={data}
+                  by={(_) => _.needs_community_currently}
+                  label={Meal_winterizationPdm.options.needs_community_currently}
+                />
+              </SlidePanel>
+              </SlidePanel>
+            </Div>
+            <Div column sx={{maxHeight: '33%'}}>
+              <SlidePanel>
+                <ChartPieWidgetBy
+                  title={m.mealMonitoringPdm.cashOrKind}
+                  filter={(_) => _.cash_modality_inkind === 'yes'}
+                  data={data}
+                  sx={{mb: 1}}
+                />
+                <ChartBarSingleBy
+                  data={data}
+                  by={(_) => _.cash_modality_inkind}
+                  label={Meal_winterizationPdm.options.cash_modality_inkind}
+                />
+              </SlidePanel>
+              <SlidePanel>
+                <ChartPieWidgetBy
+                  title={m.mealMonitoringPdm.spent}
+                  filter={(_) => _.spent_cash_assistance_received === 'yes'}
+                  data={data}
+                  sx={{mb: 1}}
+                />
+                <ChartBarSingleBy
+                  data={data}
+                  by={(_) => _.spent_cash_assistance_received}
+                  label={Meal_winterizationPdm.options.amount_cash_received_correspond}
+                />
+              </SlidePanel>
+              <SlidePanel>
+                <SlidePanel title={m.mealMonitoringPdm.fuelCommon}>
+                  <ChartBarMultipleBy
+                    data={data}
+                    by={(_) => _.type_fuel_most}
+                    label={Meal_winterizationPdm.options.type_fuel_most}
+                  />
+                </SlidePanel>
+                <SlidePanel title={m.mealMonitoringPdm.manage}>
+                  <ChartBarSingleBy
+                    data={data}
+                    by={(_) => _.manage_solid_fuel}
+                    label={Meal_winterizationPdm.options.were_informed_timeframe}
+                  />
+                </SlidePanel>
+                <SlidePanel title={m.mealMonitoringPdm.longCover}>
+                  <ChartBarSingleBy
+                    data={data}
+                    by={(_) => _.enough_hh_winter_season_cover}
+                    label={Meal_winterizationPdm.options.time_elapsed_registration}
+                  />
+                </SlidePanel>
+              </SlidePanel>
+              <SlidePanel>
+                <ChartPieWidgetBy
+                  title={m.mealMonitoringPdm.isEnough}
+                  filter={(_) => _.enough_hh_winter_season === 'yes'}
+                  data={data}
+                  sx={{mb: 1}}
+                />
+                <ChartBarSingleBy
+                  data={data}
+                  by={(_) => _.enough_hh_winter_season}
+                  label={Meal_winterizationPdm.options.any_member_household}
+                />
+              </SlidePanel>
+              <SlidePanel>
+                <ChartPieWidgetBy
+                  title={m.mealMonitoringPdm.access}
+                  filter={(_) => _.access_basic_facilities === 'yes'}
+                  data={data}
+                  sx={{mb: 1}}
+                />
+                <ChartBarSingleBy
+                  data={data}
+                  by={(_) => _.access_basic_facilities}
+                  label={Meal_winterizationPdm.options.any_member_household}
+                />
+              </SlidePanel>
+            </Div>
+            <Div column>
               <SlidePanel title={m.mealMonitoringPdm.feedback}>
                 <ChartPieWidgetBy
                   dense
@@ -156,48 +321,25 @@ export const MealWinterizationDashboard = () => {
                 />
                 <ChartPieWidgetBy
                   dense
+                  title={m.mealMonitoringPdm.feelSafe}
+                  data={data}
+                  filter={(_) => _.feel_safe_travelling === 'rcyc' || _.feel_safe_travelling === 'rcnt'}
+                  filterBase={(_) =>
+                    _.feel_safe_travelling === 'rcyc' ||
+                    _.feel_safe_travelling === 'rcnr' ||
+                    _.feel_safe_travelling === 'rcmy' ||
+                    _.feel_safe_travelling === 'rcdk' ||
+                    _.feel_safe_travelling === 'rcna' ||
+                    _.feel_safe_travelling === 'rcnt'
+                  }
+                />
+                <ChartPieWidgetBy
+                  dense
                   title={m.mealMonitoringPdm.problems}
                   data={data}
                   filter={(_) => _.experience_problems === 'yes'}
                 />
               </SlidePanel>
-              <Panel savableAsImg expendable title={m.location}>
-                <PanelBody>
-                  <MapSvgByOblast
-                    sx={{maxWidth: 480, margin: 'auto'}}
-                    fillBaseOn="value"
-                    data={data}
-                    getOblast={(_) => mapOblast[_.ben_det_oblast!]}
-                    value={(_) => true}
-                    base={(_) => _.ben_det_oblast !== undefined}
-                  />
-                </PanelBody>
-              </Panel>
-              <Panel title={m.ageGroup}>
-                <PanelBody>
-                  <AgeGroupTable
-                    tableId="pdm-dashboard"
-                    persons={persons}
-                    enableDisplacementStatusFilter
-                    enablePwdFilter
-                  />
-                </PanelBody>
-              </Panel>
-              <SlidePanel title={m.project}>
-                <ChartBarSingleBy data={data} by={(_) => _.donor} label={Meal_winterizationPdm.options.donor} />
-              </SlidePanel>
-              <SlidePanel title={m.mealMonitoringPdm.pdmType}>
-                <ChartBarSingleBy data={data} by={(_) => _.pdmtype} label={Meal_winterizationPdm.options.pdmtype} />
-              </SlidePanel>
-              <SlidePanel title={m.mealMonitoringPdm.fuelCommon}>
-                <ChartBarMultipleBy
-                  data={data}
-                  by={(_) => _.type_fuel_most}
-                  label={Meal_winterizationPdm.options.type_fuel_most}
-                />
-              </SlidePanel>
-            </Div>
-            <Div column sx={{maxHeight: '50%'}}>
               <SlidePanel title={m.mealMonitoringPdm.betterInform}>
                 <ChartBarMultipleBy
                   data={data}
@@ -205,54 +347,33 @@ export const MealWinterizationDashboard = () => {
                   label={Meal_winterizationPdm.options.better_inform_distribution}
                 />
               </SlidePanel>
-              <SlidePanel title={m.mealMonitoringPdm.cashOrKind}>
-                <ChartBarSingleBy
+              <SlidePanel>
+              <SlidePanel>
+                <ChartPieWidgetBy
+                  title={m.mealMonitoringPdm.inAdvance}
+                  filter={(_) => _.informed_amount_cash_receive === 'yes'}
                   data={data}
-                  by={(_) => _.cash_modality_inkind}
-                  label={Meal_winterizationPdm.options.cash_modality_inkind}
+                  sx={{mb: 1}}
                 />
-              </SlidePanel>
-              <SlidePanel title={m.mealMonitoringPdm.inAdvance}>
                 <ChartBarSingleBy
                   data={data}
                   by={(_) => _.informed_amount_cash_receive}
                   label={Meal_winterizationPdm.options.were_informed_timeframe}
                 />
               </SlidePanel>
-              <SlidePanel title={m.mealMonitoringPdm.assistanceCorrespond}>
+              <SlidePanel>
+                <ChartPieWidgetBy
+                  title={m.mealMonitoringPdm.assistanceCorrespond}
+                  filter={(_) => _.amount_received_correspond === 'yes'}
+                  data={data}
+                  sx={{mb: 1}}
+                />
                 <ChartBarSingleBy
                   data={data}
                   by={(_) => _.amount_received_correspond}
                   label={Meal_winterizationPdm.options.were_informed_timeframe}
                 />
               </SlidePanel>
-              <SlidePanel title={m.mealMonitoringPdm.isEnough}>
-                <ChartBarSingleBy
-                  data={data}
-                  by={(_) => _.enough_hh_winter_season}
-                  label={Meal_winterizationPdm.options.any_member_household}
-                />
-              </SlidePanel>
-              <SlidePanel title={m.mealMonitoringPdm.longCover}>
-                <ChartBarSingleBy
-                  data={data}
-                  by={(_) => _.enough_hh_winter_season_cover}
-                  label={Meal_winterizationPdm.options.time_elapsed_registration}
-                />
-              </SlidePanel>
-              <SlidePanel title={m.mealMonitoringPdm.access}>
-                <ChartBarSingleBy
-                  data={data}
-                  by={(_) => _.access_basic_facilities}
-                  label={Meal_winterizationPdm.options.any_member_household}
-                />
-              </SlidePanel>
-              <SlidePanel title={m.mealMonitoringPdm.priorityNeeds}>
-                <ChartBarMultipleBy
-                  data={data}
-                  by={(_) => _.needs_community_currently}
-                  label={Meal_winterizationPdm.options.needs_community_currently}
-                />
               </SlidePanel>
             </Div>
           </Div>
