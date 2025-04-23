a171390d9e2e3540632b943cd8e31000da59c379
Pavlo Boiko <pavlo@boiko.work>
2025-04-23 10:12:58 +0300

    hide Sidepanel's EORE menu item

--- packages/infoportal-client/src/features/Hdp/HdpSidebar.tsx
+++ packages/infoportal-client/src/features/Hdp/HdpSidebar.tsx
@@ -31,15 +31,17 @@ export const HdpSidebar: FC = () => {
             <SidebarKoboLink key={name} name={name} path={generatePath(hdpIndex.victimAssistance.form.path, {name})} />
           ))}
         </SidebarSection>
-        <SidebarSection title={m.riskEducation.sectionTitle}>
-          <NavLink to={hdpIndex.riskEducation.dashboard.path}>
-            {({isActive}) => (
-              <SidebarItem icon="insights" active={isActive}>
-                {m.dashboard}
-              </SidebarItem>
-            )}
-          </NavLink>
-        </SidebarSection>
+        {process?.env?.NODE_ENV === 'development' && (
+          <SidebarSection title={m.riskEducation.sectionTitle}>
+            <NavLink to={hdpIndex.riskEducation.dashboard.path}>
+              {({isActive}) => (
+                <SidebarItem icon="insights" active={isActive}>
+                  {m.dashboard}
+                </SidebarItem>
+              )}
+            </NavLink>
+          </SidebarSection>
+        )}
       </SidebarBody>
     </Sidebar>
   )
