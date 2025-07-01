357b9bd615f833ca9d90e206436c0243d74d611f
Pavlo Boiko <pavlo@boiko.work>
2025-07-01 16:37:06 +0300

    [fix][client] fix blank page on /legal

--- packages/infoportal-client/src/features/Legal/Legal.tsx
+++ packages/infoportal-client/src/features/Legal/Legal.tsx
@@ -1,5 +1,5 @@
-import type {FC} from 'react'
-import {Navigate, Route, Routes, Outlet} from 'react-router-dom'
+import {useEffect, type FC} from 'react'
+import {Navigate, Route, Routes} from 'react-router-dom'
 
 import {appFeaturesIndex} from '@/features/appFeatureId'
 import {Layout} from '@/shared/Layout'
@@ -10,26 +10,24 @@ import {Dashboard as IndividualAidDashboard, Data as IndividualAidData} from './
 import LegalSidebar from './LegalSidebar'
 
 const Legal: FC = () => {
+  useEffect(() => {
+    // force routing handover from Next's to React's router to fix the blank page
+    if (window.location.hash === '') {
+      window.location.replace(`legal#/${pages.individualLegalAid.slug}`)
+    }
+  }, [])
+
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Layout
-            title={appFeaturesIndex.legal.name}
-            sidebar={<LegalSidebar />}
-            header={<AppHeader id="app-header" />}
-          />
-        }
-      >
+    <Layout title={appFeaturesIndex.legal.name} sidebar={<LegalSidebar />} header={<AppHeader id="app-header" />}>
+      <Routes>
         <Route index element={<Navigate to={pages.individualLegalAid.slug} replace />} />
-        <Route path={pages.individualLegalAid.slug} Component={Outlet}>
+        <Route path={pages.individualLegalAid.slug}>
           <Route index element={<Navigate to={pages.individualLegalAid.dashboard.slug} replace />} />
           <Route path={pages.individualLegalAid.dashboard.slug} element={<IndividualAidDashboard />} />
           <Route path={pages.individualLegalAid.data.slug} element={<IndividualAidData />} />
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </Layout>
   )
 }
 
