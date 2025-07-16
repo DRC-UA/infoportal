a429faf2b18114c7d2662274b7d47095be539136
Pavlo Boiko <pavlo@boiko.work>
2025-07-16 09:55:39 +0300

    [fix][client] fix blank page on front-end nav to HDP

--- packages/infoportal-client/src/features/Hdp/Hdp.tsx
+++ packages/infoportal-client/src/features/Hdp/Hdp.tsx
@@ -1,4 +1,4 @@
-import type {FC} from 'react'
+import {useEffect, type FC} from 'react'
 import {Route, Routes} from 'react-router-dom'
 
 import {appFeaturesIndex} from '@/features/appFeatureId'
@@ -10,16 +10,25 @@ import {HdpSidebar} from './HdpSidebar'
 import {hdpIndex} from './constants'
 import {Peacebuilding} from '@/features/Hdp/Peacebuilding'
 
-export const Hdp: FC = () => (
-  <Routes>
-    <Route
-      path="/"
-      element={
-        <Layout title={appFeaturesIndex.hdp.name} sidebar={<HdpSidebar />} header={<AppHeader id="app-header" />} />
-      }
-    >
-      <Route path={`${hdpIndex.victimAssistance.slug}/*`} Component={VictimAssistance} />
-      <Route path={`${hdpIndex.peacebuilding.slug}/*`} Component={Peacebuilding} />
-    </Route>
-  </Routes>
-)
+export const Hdp: FC = () => {
+  useEffect(() => {
+    // force routing handover from Next's to React's router to fix the blank page
+    if (window.location.hash === '') {
+      window.location.replace(`hdp#/${hdpIndex.victimAssistance.slug}`)
+    }
+  }, [])
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout title={appFeaturesIndex.hdp.name} sidebar={<HdpSidebar />} header={<AppHeader id="app-header" />} />
+        }
+      >
+        <Route path={`${hdpIndex.victimAssistance.slug}/*`} Component={VictimAssistance} />
+        <Route path={`${hdpIndex.peacebuilding.slug}/*`} Component={Peacebuilding} />
+      </Route>
+    </Routes>
+  )
+}
