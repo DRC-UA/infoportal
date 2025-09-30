a306adf3815ebfe1fcb6b6ac3ee88966d2d4a216
Pavlo Boiko <pavlo@boiko.work>
2025-09-30 12:17:30 +0300

    get rid of unkown assertion

--- packages/infoportal-server/src/feature/kobo/meta/KoboMetaService.ts
+++ packages/infoportal-server/src/feature/kobo/meta/KoboMetaService.ts
@@ -187,7 +187,7 @@ export class KoboMetaService {
         orderBy: {
           date: 'desc',
         },
-      })) as unknown as IKoboMeta[] // TODO: get rid of unknown - there is a typing error needed a fix
+      })) as IKoboMeta[]
       this.log.info(`Fetch Meta from database... ${res.length} fetched in ${duration(performance.now() - t0)}`)
       return res
     },
