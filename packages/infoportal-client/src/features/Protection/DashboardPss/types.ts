f01aa319dda82fce8eb997e3a4e1890e109af66c
Pavlo Boiko <pavlo@boiko.work>
2025-10-24 16:04:32 +0300

    add initial PSS dashboard

---
+++ packages/infoportal-client/src/features/Protection/DashboardPss/types.ts
@@ -0,0 +1,11 @@
+import type {Seq} from '@axanc/ts-utils'
+
+import {Person, Protection_pss} from 'infoportal-common'
+
+interface ProtectionPssWithPersons extends Protection_pss.T {
+  persons: Person.Details[]
+}
+
+type ProtectionPssWithPersonsFlat = ProtectionPssWithPersons & Person.Details
+
+export type {ProtectionPssWithPersons, ProtectionPssWithPersonsFlat}
