45421c3754fe63a5ee5d27ca915d902defa46d70
Pavlo Boiko <pavlo@boiko.work>
2025-11-13 18:07:17 +0200

    redirect to react router

--- packages/infoportal-client/src/pages/communications.tsx
+++ packages/infoportal-client/src/pages/communications.tsx
@@ -1,15 +1,14 @@
+import type {FC} from 'react'
+
 import {ProtectRoute} from '@/core/Session/SessionContext'
 import {Communications} from '@/features/Communications'
 
-export default () => {
-  try {
-    return (
-      <ProtectRoute>
-        <Communications />
-      </ProtectRoute>
-    )
-  } catch (error) {
-    console.error('An error rendering Communications occured', error)
-    return <div>Error: {String(error)}</div>
-  }
+const CommunicationsPage: FC = () => {
+  return (
+    <ProtectRoute>
+      <Communications />
+    </ProtectRoute>
+  )
 }
+
+export default CommunicationsPage
