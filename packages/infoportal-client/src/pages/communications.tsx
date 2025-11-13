34c83df2bdd0d067f0c7d23c9cb7636643b44627
Pavlo Boiko <pavlo@boiko.work>
2025-11-13 16:27:08 +0200

    investigate comms error

--- packages/infoportal-client/src/pages/communications.tsx
+++ packages/infoportal-client/src/pages/communications.tsx
@@ -1,8 +1,15 @@
 import {ProtectRoute} from '@/core/Session/SessionContext'
 import {Communications} from '@/features/Communications'
 
-export default () => (
-  <ProtectRoute>
-    <Communications />
-  </ProtectRoute>
-)
+export default () => {
+  try {
+    return (
+      <ProtectRoute>
+        <Communications />
+      </ProtectRoute>
+    )
+  } catch (error) {
+    console.error('An error rendering Communications occured', error)
+    return <div>Error: {String(error)}</div>
+  }
+}
