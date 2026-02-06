20000eb7150c6ed985f83fecc142b5061a2ae13d
Pavlo Boiko <pavlo@boiko.work>
2026-02-06 13:31:38 +0200

    add filter to modal

--- packages/infoportal-client/src/features/Database/DatabaseNew/DatabaseNew.tsx
+++ packages/infoportal-client/src/features/Database/DatabaseNew/DatabaseNew.tsx
@@ -1,13 +1,14 @@
 import {useEffect, useState, type ReactElement} from 'react'
 import {useEffectFn} from '@alexandreannic/react-hooks-lib'
-import {Box} from '@mui/material'
+import {Box, Icon} from '@mui/material'
 
 import {useAppSettings} from '@/core/context/ConfigContext'
 import {useI18n} from '@/core/i18n'
 import {KoboFormCreate} from '@/core/sdk/server/kobo/KoboFormSdk'
 import {useIpToast} from '@/core/useToast'
 import {useDatabaseContext} from '@/features/Database/DatabaseContext'
-import {Modal, Txt} from '@/shared'
+import {IpIconBtn, Modal, Txt} from '@/shared'
+import {IpInput} from '@/shared/Input/Input'
 import {useAsync} from '@/shared/hook/useAsync'
 import {useFetcher} from '@/shared/hook/useFetcher'
 import {useFetchers} from '@/shared/hook/useFetchers'
@@ -23,6 +24,8 @@ export const DatabaseNew = ({children, onAdded}: {onAdded?: () => void; children
   const {toastHttpError} = useIpToast()
   const {formsAccessible} = useDatabaseContext()
   const accessibleFormIds = formsAccessible?.map((form) => form.id)
+  const [query, setQuery] = useState('')
+  const handleClear = () => setQuery('')
 
   useEffectFn(_server.error, toastHttpError)
   // useEffectFn(_form.error, toastHttpError)
@@ -50,46 +53,92 @@ export const DatabaseNew = ({children, onAdded}: {onAdded?: () => void; children
           setSelectedForm(undefined)
         }
       }}
-      content={_server.get?.map((server) => (
-        <Box
-          key={server.id}
-          sx={{
-            '&:not(:last-of-type)': {
-              mb: 2,
-            },
-          }}
-        >
-          <Txt size="big" bold sx={{mb: 0.5}}>
-            {server.url.replace('https://', '')}
-          </Txt>
-          <ScRadioGroup dense value={selectedForm?.uid}>
-            {_form.get[server.id]
-              ?.filter((_) => _.has_deployment)
-              .filter((form) => !accessibleFormIds?.includes(form.uid)) // do not show already connected forms
-              .map((form) => (
-                <ScRadioGroupItem
-                  dense
-                  key={form.uid}
-                  selected={form.uid === selectedForm?.uid}
-                  value={form.uid}
-                  onClick={() =>
-                    setSelectedForm({
-                      uid: form.uid,
-                      serverId: server.id,
-                    })
-                  }
-                  title={form.name}
-                  description={
-                    <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
-                      <Box>{form.deployment__submission_count}</Box>
-                      {formatDate(form.date_created)}
+      slotProps={{
+        paper: {
+          sx: {
+            height: '90%',
+            width: '90%',
+          },
+        },
+      }}
+      content={
+        <>
+          <IpInput
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder={m.filterPlaceholder}
+            endAdornment={
+              <IpIconBtn
+                disabled={!query.length}
+                aria-label={m.clearFilter}
+                title={m.clearFilter}
+                size="small"
+                onClick={handleClear}
+              >
+                close
+              </IpIconBtn>
+            }
+          />
+          {_server.get?.map((server) => (
+            <Box
+              key={server.id}
+              sx={{
+                '&:not(:last-of-type)': {
+                  mb: 2,
+                },
+              }}
+            >
+              <Txt size="big" bold sx={{mb: 0.5}}>
+                {server.url.replace('https://', '')}
+              </Txt>
+              <ScRadioGroup dense value={selectedForm?.uid}>
+                {(() => {
+                  const forms = _form.get[server.id]
+                    ?.filter((_) => _.has_deployment)
+                    .filter((form) => !accessibleFormIds?.includes(form.uid)) // do not show already connected forms
+                    .filter((form) => form.name.toLowerCase().includes(query.toLowerCase()))
+                    .map((form) => (
+                      <ScRadioGroupItem
+                        dense
+                        key={form.uid}
+                        selected={form.uid === selectedForm?.uid}
+                        value={form.uid}
+                        onClick={() =>
+                          setSelectedForm({
+                            uid: form.uid,
+                            serverId: server.id,
+                          })
+                        }
+                        title={form.name}
+                        description={
+                          <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
+                            <Box>{form.deployment__submission_count}</Box>
+                            {formatDate(form.date_created)}
+                          </Box>
+                        }
+                      />
+                    ))
+
+                  return forms && forms.length > 0 ? (
+                    forms
+                  ) : (
+                    <Box
+                      sx={{
+                        textAlign: 'center',
+                        mt: 2,
+                        color: (t) => t.palette.text.disabled,
+                      }}
+                    >
+                      <Icon sx={{fontSize: '3em !important'}}>block</Icon>
+                      <Box>{m.missingOrFaulty}</Box>
                     </Box>
-                  }
-                />
-              ))}
-          </ScRadioGroup>
-        </Box>
-      ))}
+                  )
+                })()}
+              </ScRadioGroup>
+            </Box>
+          ))}
+        </>
+      }
     >
       {children}
     </Modal>
