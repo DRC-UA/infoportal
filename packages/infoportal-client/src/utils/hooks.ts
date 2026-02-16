936688d3136a4aae74cb9dcc407a873fca0b4a7c
Pavlo Boiko <79691246+p-d-boiko@users.noreply.github.com>
2026-02-16 17:36:02 +0200

    [feat][meal][pdm] Combine partial answers in VA PDM (#366)

--- packages/infoportal-client/src/utils/hooks.ts
+++ packages/infoportal-client/src/utils/hooks.ts
@@ -1,9 +1,42 @@
-import {useI18n} from '@/core/i18n'
+import {useEffect, useCallback} from 'react'
+import {match} from '@axanc/ts-utils'
 
-export const usePlurals = (nounObject: Record<Intl.LDMLPluralRule, string>) => {
+import {type KoboFormName} from 'infoportal-common'
+
+import {useI18n} from '@/core/i18n'
+import {useKoboSchemaContext} from '@/features/KoboSchema/KoboSchemaContext'
+
+const usePlurals = (nounObject: Record<Intl.LDMLPluralRule, string>) => {
   const {currentLang} = useI18n()
   const localRules = new Intl.PluralRules(currentLang, {type: 'cardinal'})
   const nounMap = new Map(Object.entries(nounObject))
 
   return (number: number) => nounMap.get(localRules.select(number))
 }
+
+const useKoboTranslations = (formName: KoboFormName, langCases: {uk: 0; en: 1} | {uk: 1; en: 0} = {uk: 0, en: 1}) => {
+  const {currentLang} = useI18n()
+  const schemaContext = useKoboSchemaContext({autoFetch: [formName]})
+  const gbvConceptsSchema = schemaContext.byName[formName].get
+
+  const getOptionTranslations = useCallback(
+    (option: string) => {
+      return gbvConceptsSchema?.helper.getOptionsByQuestionName(option)?.map(({name}) => ({
+        value: name,
+        label: gbvConceptsSchema.translate.choice(option, name) ?? name,
+      }))
+    },
+    [gbvConceptsSchema],
+  )
+
+  useEffect(() => {
+    schemaContext.setLangIndex(match(currentLang).cases(langCases).exhaustive())
+  }, [currentLang])
+
+  return {
+    translateOption: getOptionTranslations,
+    translateField: gbvConceptsSchema?.translate.question,
+  }
+}
+
+export {useKoboTranslations, usePlurals}
