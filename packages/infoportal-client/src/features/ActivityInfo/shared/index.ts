c98121722544b9ada09b46e6e8bdac0900937581
Pavlo Boiko <pavlo@boiko.work>
2026-05-11 13:20:40 +0300

    remove missing export

--- packages/infoportal-client/src/features/ActivityInfo/shared/index.ts
+++ packages/infoportal-client/src/features/ActivityInfo/shared/index.ts
@@ -1,6 +1,6 @@
 export {AiMapper} from './AiMapper'
 export {AiTable} from './AiTable'
-export {ALERT, PROGRAM_PREFIXES} from './constants'
+export {ALERT} from './constants'
 export {useMetaFetcher} from './hooks'
 export {AiType51aMonitoring, type AiTableProps, type AiTableData, type Bundle} from './types'
 export {
